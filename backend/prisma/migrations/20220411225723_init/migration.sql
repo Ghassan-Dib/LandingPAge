6d5bd089146bf98e6e669ffc225b77b49760c70f
Ghassan-Dib <gid01@mail.aub.edu>
2022-05-05 16:48:02 +0300

    fix: eslint errors

--- backend/prisma/migrations/20220411225723_init/migration.sql
+++ backend/prisma/migrations/20220411225723_init/migration.sql
@@ -1,7 +1,8 @@
 /*
   Warnings:
 
-  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password` to the `User` table without a default value. 
+  This is not possible if the table is not empty.
 
 */
 -- AlterTable
